da44eb5a0bcc955269751284b0b671c7da793a46
fernand21 <farevalo210@gmail.com>
2026-09-21 21:59:34 -0500

    Update InstallerLab Magic API token

--- assets/analytics-config.js
+++ assets/analytics-config.js
@@ -3,7 +3,7 @@
 // service_role/secret key in this static site or in a generated installer.
 window.INSTALLERLAB_ANALYTICS_CONFIG = Object.freeze({
   magicApiUrl: 'https://b4xapp.com/aplicaciones/installerlab/api.php',
-  magicApiReadToken: 'magic_af65ec2e57161dc5f5ca25687f40f0d3c8e9c616235be382eb50d1016b0ae542',
+  magicApiReadToken: 'magic_87b9a72c228001c616f8a5f300b13eed09e8b162d0a7304726c827b41783e5b7',
   url: 'https://kjbmheqsebikpvamotov.supabase.co',
   publishableKey: 'sb_publishable_0fA4y3-87oHZEaA2zZZGUQ_d__9tteR',
 
